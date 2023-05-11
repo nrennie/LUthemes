dc9c7a36df3495d4ad6e76dca44f4255f903747d
nrennie <nrennie35@gmail.com>
2023-05-11 23:10:21 +0100

    load local fonts instead

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,14 @@
 .onLoad <- function(libname, pkgname) {
-  sysfonts::font_add_google("Roboto", "Roboto")
-  sysfonts::font_add_google("Roboto Slab", "Roboto Slab")
+  sysfonts::font_add(
+    family = "Roboto",
+    regular = system.file("fonts", "Roboto", "Roboto-Regular.ttf", package = "LUthemes"),
+    italic = system.file("fonts", "Roboto", "Roboto-Italic.ttf", package = "LUthemes"),
+    bold = system.file("fonts", "Roboto", "Roboto-Bold.ttf", package = "LUthemes")
+  )
+  sysfonts::font_add(
+    family = "Roboto Slab",
+    regular = system.file("fonts", "RobotoSlab", "RobotoSlab-Regular.ttf", package = "LUthemes"),
+    bold = system.file("fonts", "RobotoSlab", "RobotoSlab-Bold.ttf", package = "LUthemes")
+  )
   showtext::showtext_auto()
 }
